8abefc44878817d12a07aa8b3a6707c0e3d8c729
Marc Hein <me@marc-hein.de>
2026-01-26 14:29:58 +0100

    Initial Commit

---
+++ WhateverClock/WhateverClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhateverClockApp.swift
+//  WhateverClock
+//
+//  Created by Marc Hein on 26.01.26.
+//
+
+import SwiftUI
+
+@main
+struct WhateverClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
